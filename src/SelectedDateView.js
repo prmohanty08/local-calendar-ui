5158dfbd7fb739fb9aed0036f65d269889b28c55
Priyaranjan Mohanty <prmohanty08@gmail.com>
2024-04-23 06:02:36 -0500

    update code structure

--- src/SelectedDateView.js
+++ src/SelectedDateView.js
@@ -8,6 +8,7 @@ import { getWeekdayLabel } from './CalendarMasterData.js'
 
 import { useSelectedDateTime } from './DateTimeContext';
 import Moon from './Moon.js';
+import VerticalBanner from './VerticalBanner.js';
 
 const SelectedDateView = () => {
     const { selectedDate } = useSelectedDateTime();
@@ -16,12 +17,13 @@ const SelectedDateView = () => {
     if (selectedDate && selectedDate.date) {
         return (
             <div className="column dark-pink-red center-aligned-content">
+                <VerticalBanner />
                 <p>ଓଡି଼ଶାରେ ଆଜି</p>
                 <Moon></Moon>
                 <p className='the-occasion'>
-                    {monthData ? monthData.occasions[selectedDate.date - 1]?.tithi : ''}
+                    {monthData && monthData.occasions ? monthData.occasions[selectedDate.date - 1]?.tithi : ''}
                     <br />
-                    {monthData ? monthData.occasions[selectedDate.date - 1]?.naxatra : ''}
+                    {monthData && monthData.occasions ? monthData.occasions[selectedDate.date - 1]?.naxatra : ''}
                 </p>
                 <p className='the-day'>{getWeekdayLabel(selectedDate.weekday - 1, 'OR')}</p>
                 <p className='the-date'>{convertToOdia(selectedDate.date)}</p>
---
+++ src/VerticalBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './VerticalBanner.css';
+
+const VerticalBanner = () => {
+    return (
+        <div className="vertical-banner">
+            <div className="vertical-banner-text">
+                ପରୀକ୍ଷା ମୂଳକ ପ୍ରାରମ୍ଭିକ ସଂସ୍କରଣ &nbsp; &nbsp; [ ଆପଣ କିଛି ତ୍ରୁଟି ଦେଖିପାରନ୍ତି ]
+            </div>
+        </div>
+    )
+}
+
+export default VerticalBanner;
